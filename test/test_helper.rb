2784cf360c7b5279044d1984e1cd08bf512c8a60
David Furber <furberd@gmail.com>
2015-03-02 08:17:14 -0500

    and another merge conflict

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -101,6 +101,7 @@ end
 
 class Door
   include ArDocStore::EmbeddableModel
+  self.primary_key = :id
   attribute :id, :uuid
   enumerates :door_type, multiple: true, values: %w{single double french sliding push pull}
   attribute :open_handle,  as: :enumeration, multiple: true, values: %w{push pull plate knob handle}
