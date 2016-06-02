74b1d29c870b71f07d1338e682eb2d9887388f89
Dave Shaffer <dave.shaffer@gmail.com>
2016-06-02 11:20:34 -0400

    Remove now-unnecessary implementation details in DateTimeAttribute

--- lib/ar_doc_store/attribute_types/datetime_attribute.rb
+++ lib/ar_doc_store/attribute_types/datetime_attribute.rb
@@ -1,42 +1,15 @@
 module ArDocStore
   module AttributeTypes
     class DatetimeAttribute < BaseAttribute
-      def build
-        attribute = @attribute
-        load_method = load
-        dump_method = dump
-        default_value = default
-        json_column = :data
-        model.class_eval do
-          add_ransacker(attribute, 'timestamp')
-          define_method attribute.to_sym, -> {
-            value = read_store_attribute(json_column, attribute)
-            if value
-              value.public_send(dump_method)
-            elsif default_value
-              write_default_store_attribute(attribute, default_value)
-              default_value
-            end
-          }
-          define_method "#{attribute}=".to_sym, -> (value) {
-            if value.blank?
-              write_store_attribute(json_column, attribute, nil)
-            else
-              write_store_attribute(json_column, attribute, value.public_send(load_method))
-            end
-          }
-        end
-      end
-
       def type
         :datetime
       end
 
-      def dump
+      def load
         :to_time
       end
 
-      def load
+      def dump
         :to_s
       end
     end
